70b26b5861a20d23f7392af555774f38008eb5c1
sandaluthushan20 <sandaluthushan20@gmail.com>
2024-05-28 14:19:55 +0530

    second commit

--- mobile-sell/src/Components/LoginPage.jsx
+++ mobile-sell/src/Components/LoginPage.jsx
@@ -5,24 +5,29 @@ import { Container, Form, Button } from 'react-bootstrap';
 
 const LoginPage = () => {
   return (
-    <Container className="login-container">
-      <h2 className="login-title">Login</h2>
+
+    <div className='login'>
+       <Container className="login-container">
+        <div className='login-fo'>
+        <h2 className="login-title">Login</h2>
       <Form className="login-form">
         <Form.Group controlId="formBasicEmail">
           <Form.Label className="login-label">Email address</Form.Label>
           <Form.Control type="email" placeholder="Enter email" className="login-input" />
         </Form.Group>
-
         <Form.Group controlId="formBasicPassword">
           <Form.Label className="login-label">Password</Form.Label>
           <Form.Control type="password" placeholder="Password" className="login-input" />
         </Form.Group>
-
         <Button variant="primary" type="submit" className="login-button">
           Submit
         </Button>
       </Form>
+        </div>
     </Container>
+
+    </div>
+   
   );
 };
 
